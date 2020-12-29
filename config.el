3929cc780de2f5532342f5972ec36e40c72be7d9
전병완 <ian@Ians-MacBookPro.local>
2020-12-29 09:27:50 +0900

    Change theme to zenburn

--- config.el
+++ config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-zenburn)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -38,7 +38,6 @@
 ;; Fix issues after installing Emacs 27.1 on macOS
 (setq default-directory "~/")
 (setq command-line-default-directory "~/")
-
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
