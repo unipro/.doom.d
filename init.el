9832eb3532ad0e8ce5d6a6cd82e8eda1ca46f656
Byungwan Jun <unipro.kr@gmail.com>
2021-03-16 13:16:00 +0900

    Use ido instead of ivy

--- init.el
+++ init.el
@@ -22,8 +22,8 @@
        :completion
        (company +tng)      ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       ido               ; the other *other* search engine...
+       ;;ivy               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
