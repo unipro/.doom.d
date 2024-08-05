e993d136b653803d8a4664ba46ce4c79349be203
Byungwan Jun <unipro.kr@gmail.com>
2024-08-06 05:38:10 +0900

    disable web, zig

--- init.el
+++ init.el
@@ -191,9 +191,9 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       web               ; the tubes
+       ;;web               ; the tubes
        yaml              ; JSON, but readable
-       zig               ; C, but simpler
+       ;;zig               ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
