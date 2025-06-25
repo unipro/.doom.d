4ba7c878fda01624d6b8fa10d8320fc2f521664d
Byungwan Jun <unipro.kr@gmail.com>
2025-06-25 17:37:42 +0900

    fix(packages): update copilot package recipe to include dist files

--- packages.el
+++ packages.el
@@ -41,7 +41,7 @@
 
 ;; copilot
 (package! copilot
-  :recipe (:host github :repo "copilot-emacs/copilot.el"))
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
 (package! copilot-chat
   :recipe (:host github :repo "chep/copilot-chat.el" :files ("*.el")))
 
